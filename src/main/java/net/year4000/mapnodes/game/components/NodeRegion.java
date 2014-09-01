85a99b9a75bbe423825d30d1fc7d5e493fa762b0
Joshua Rodriguez <ewized@gmail.com>
2014-09-01 07:54:28 -0700

    Add comment to node region

--- src/main/java/net/year4000/mapnodes/game/components/NodeRegion.java
+++ src/main/java/net/year4000/mapnodes/game/components/NodeRegion.java
@@ -5,6 +5,7 @@ import net.year4000.mapnodes.exceptions.InvalidJsonException;
 import net.year4000.mapnodes.utils.Validator;
 
 public final class NodeRegion implements GameRegion, Validator {
+    // todo convert the list of points to a set for fast look up
 
 
     @Override
