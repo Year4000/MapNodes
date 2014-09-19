d4a5edcd5d06aab875dc30b46a012682adb01e1d
Joshua Rodriguez <ewized@gmail.com>
2014-09-18 20:35:56 -0700

    Fix death messages

--- src/main/java/net/year4000/mapnodes/addons/modules/misc/DeathMessages.java
+++ src/main/java/net/year4000/mapnodes/addons/modules/misc/DeathMessages.java
@@ -61,16 +61,25 @@ public class DeathMessages extends Addon implements Listener {
         }
     }
 
-    /** Add the players names with their team color. */
-    public String replace(Player k, Player d, String message, Object... args) {
+    public String replace(Player k, Player d, Player viewer, String message) {
         GameManager gm = MapNodes.getCurrentGame();
 
         return MessageUtil.message(
-            "&7&o" + message,
+            "&7&o" + Msg.locale(viewer, message,
+                gm.getPlayer(d).getPlayerColor() + "&7&o",
+                gm.getPlayer(k).getPlayerColor() + "&7&o"
+            ));
+    }
+
+    public String replace(Player k, Player d, Player viewer, String message, Object args) {
+        GameManager gm = MapNodes.getCurrentGame();
+
+        return MessageUtil.message(
+            "&7&o" + Msg.locale(viewer, message,
             gm.getPlayer(d).getPlayerColor() + "&7&o",
             gm.getPlayer(k).getPlayerColor() + "&7&o",
-            args
-        );
+            String.valueOf(args)
+        ));
     }
 
     /** Format the item so humans can read it. */
@@ -89,41 +98,41 @@ public class DeathMessages extends Addon implements Listener {
     public String getMessage(Player viewer, Player d, Player k) {
         switch (d.getLastDamageCause().getCause()) {
             case BLOCK_EXPLOSION:
-                return replace(k, d, Msg.locale(viewer, "death.block_explosion"));
+                return replace(k, d, viewer, "death.block_explosion");
             case DROWNING:
-                return replace(k, d, Msg.locale(viewer, "death.drowing"));
+                return replace(k, d, viewer, "death.drowing");
             case FALL:
-                return replace(k, d, Msg.locale(viewer, "death.fall"));
+                return replace(k, d, viewer, "death.fall");
             case ENTITY_ATTACK:
-                return replace(k, d, Msg.locale(viewer, "death.entity_attack", humanItem(k.getItemInHand())));
+                return replace(k, d, viewer, "death.entity_attack", humanItem(k.getItemInHand()));
             case ENTITY_EXPLOSION:
-                return replace(k, d, Msg.locale(viewer, "death.entity_explosion"));
+                return replace(k, d, viewer, "death.entity_explosion");
             case FALLING_BLOCK:
-                return replace(k, d, Msg.locale(viewer, "death.falling_block"));
+                return replace(k, d, viewer, "death.falling_block");
             case FIRE:
             case FIRE_TICK:
-                return replace(k, d, Msg.locale(viewer, "death.fire"));
+                return replace(k, d, viewer, "death.fire");
             case LAVA:
-                return replace(k, d, Msg.locale(viewer, "death.lava"));
+                return replace(k, d, viewer, "death.lava");
             case POISON:
-                return replace(k, d, Msg.locale(viewer, "death.poison"));
+                return replace(k, d, viewer, "death.poison");
             case LIGHTNING:
-                return replace(k, d, Msg.locale(viewer, "death.lighting"));
+                return replace(k, d, viewer, "death.lighting");
             case PROJECTILE:
                 int distance = (int)Math.sqrt(k.getLocation().distanceSquared(d.getLocation()));
-                return replace(k, d, Msg.locale(viewer, "death.projectile", String.valueOf(distance)));
+                return replace(k, d, viewer, "death.projectile", String.valueOf(distance));
             case MAGIC:
-                return replace(k, d, Msg.locale(viewer, "death.magic"));
+                return replace(k, d, viewer, "death.magic");
             case SUFFOCATION:
-                return replace(k, d, Msg.locale(viewer, "death.suffocation"));
+                return replace(k, d, viewer, "death.suffocation");
             case THORNS:
-                return replace(k, d, Msg.locale(viewer, "death.thorns"));
+                return replace(k, d, viewer, "death.thorns");
             case WITHER:
-                return replace(k, d, Msg.locale(viewer, "death.wither"));
+                return replace(k, d, viewer, "death.wither");
             case VOID:
-                return replace(k, d, Msg.locale(viewer, "death.void"));
+                return replace(k, d, viewer, "death.void");
             default:
-                return replace(k, d, Msg.locale(viewer, "death.default"));
+                return replace(k, d, viewer, "death.default");
         }
     }
 }
