0976bafe8b806c683925e9d399b476645873f5a2
Joshua Rodriguez <ewized@gmail.com>
2014-09-24 08:08:58 -0700

    Basics Deathmatch to test code.

--- src/main/java/net/year4000/mapnodes/gamemodes/deathmatch/Deathmatch.java
+++ src/main/java/net/year4000/mapnodes/gamemodes/deathmatch/Deathmatch.java
@@ -2,8 +2,10 @@ package net.year4000.mapnodes.gamemodes.deathmatch;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import net.year4000.mapnodes.api.MapNodes;
 import net.year4000.mapnodes.api.game.modes.GameMode;
 import net.year4000.mapnodes.api.game.modes.GameModeInfo;
+import net.year4000.mapnodes.game.NodeGame;
 import net.year4000.mapnodes.gamemodes.GameModeTemplate;
 
 @GameModeInfo(
@@ -15,4 +17,14 @@ import net.year4000.mapnodes.gamemodes.GameModeTemplate;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Deathmatch extends GameModeTemplate implements GameMode {
+    private long startTime;
+    private long endTime;
+
+    public void setUpGoals() {
+        MapNodes.getCurrentGame().getTeams().values().forEach(team -> {
+            // todo dont use node game must use GameManger
+            ((NodeGame) MapNodes.getCurrentGame()).addDyamicGoal(team.getId(), team.getDisplayName(), 0);
+        });
+    }
+
 }
--- src/main/java/net/year4000/mapnodes/gamemodes/deathmatch/DeathmatchConfig.java
+++ src/main/java/net/year4000/mapnodes/gamemodes/deathmatch/DeathmatchConfig.java
@@ -1,14 +1,21 @@
 package net.year4000.mapnodes.gamemodes.deathmatch;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import net.year4000.mapnodes.api.game.modes.GameModeConfig;
 import net.year4000.mapnodes.api.game.modes.GameModeConfigName;
 import net.year4000.mapnodes.exceptions.InvalidJsonException;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Data
 @GameModeConfigName("deathmatch")
 public class DeathmatchConfig implements GameModeConfig {
+    @SerializedName("time_limit")
+    private int timeLimit = 60;
+
     @Override
     public void validate() throws InvalidJsonException {
+        checkArgument(timeLimit > 0);
     }
 }
