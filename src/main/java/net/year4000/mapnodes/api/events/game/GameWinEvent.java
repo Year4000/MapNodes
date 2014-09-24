1bbe137f83d27b5e381685ee36998441b075402a
Joshua Rodriguez <ewized@gmail.com>
2014-09-24 08:59:27 -0700

    Game win event include the game refrence

--- src/main/java/net/year4000/mapnodes/api/events/game/GameWinEvent.java
+++ src/main/java/net/year4000/mapnodes/api/events/game/GameWinEvent.java
@@ -1,8 +1,12 @@
 package net.year4000.mapnodes.api.events.game;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.year4000.mapnodes.api.events.EventPlus;
+import net.year4000.mapnodes.api.game.GameManager;
 
+@Data
 @EqualsAndHashCode(callSuper = false)
 public class GameWinEvent extends EventPlus {
+    protected GameManager game;
 }
