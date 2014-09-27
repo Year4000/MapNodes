c86074939e2d4de47f69b74ca7c71f86b2464be5
Joshua Rodriguez <ewized@gmail.com>
2014-09-27 12:03:38 -0700

    Added Skywars gamemode for #12

--- src/main/java/net/year4000/mapnodes/gamemodes/skywars/Skywars.java
+++ src/main/java/net/year4000/mapnodes/gamemodes/skywars/Skywars.java
@@ -2,11 +2,31 @@ package net.year4000.mapnodes.gamemodes.skywars;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import net.year4000.mapnodes.api.MapNodes;
 import net.year4000.mapnodes.api.events.game.GameLoadEvent;
+import net.year4000.mapnodes.api.events.game.GameStartEvent;
+import net.year4000.mapnodes.api.events.game.GameWinEvent;
+import net.year4000.mapnodes.api.events.player.*;
+import net.year4000.mapnodes.api.events.team.GameTeamWinEvent;
 import net.year4000.mapnodes.api.game.modes.GameMode;
 import net.year4000.mapnodes.api.game.modes.GameModeInfo;
+import net.year4000.mapnodes.game.NodeGame;
+import net.year4000.mapnodes.game.NodePlayer;
+import net.year4000.mapnodes.game.NodeTeam;
+import net.year4000.mapnodes.game.system.Spectator;
 import net.year4000.mapnodes.gamemodes.GameModeTemplate;
+import net.year4000.mapnodes.messages.Msg;
+import net.year4000.mapnodes.utils.SchedulerUtil;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @GameModeInfo(
     name = "Skywars",
@@ -14,12 +34,115 @@ import org.bukkit.event.EventHandler;
     config = SkywarsConfig.class
 )
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class Skywars extends GameModeTemplate implements GameMode {
     private SkywarsConfig gameModeConfig;
+    private NodeGame game;
+    private NodeTeam team;
+    private Iterator<Location> spawns;
+    private List<String> alive = new ArrayList<>();
+    private List<String> dead = new ArrayList<>();
 
     @EventHandler
     public void onLoad(GameLoadEvent event) {
         gameModeConfig = (SkywarsConfig) getConfig();
+        game = (NodeGame) event.getGame();
+        team = game.getTeams().get(gameModeConfig.getPlayersTeam());
+
+        // Add requirements | Their must be at least 2 players to start Skywars
+        game.getStartControls().add(() -> team.getPlayers().size() >= 2);
+
+        // Create the iterator of spawns
+        spawns = team.getSpawns().iterator();
+    }
+
+    /** Set the player's spawn to the next spawn in the list */
+    @EventHandler
+    public void onPlayerJoin(GamePlayerStartEvent event) {
+        event.setImmortal(false);
+        event.setSpawn(spawns.next());
+    }
+
+    /** Don't allow players to select teams when the game started */
+    @EventHandler
+    public void onTeamSelect(GamePlayerJoinTeamEvent event) {
+        if (event.getTo() instanceof Spectator) return;
+
+        if (MapNodes.getCurrentGame().getStage().isPlaying()) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(Msg.locale(event.getPlayer(), "skywars.join.playing"));
+        }
+    }
+
+    /** Disable menu when game started */
+    @EventHandler
+    public void onPlayerJoin(GamePlayerJoinEvent event) {
+        if (MapNodes.getCurrentGame().getStage().isPlaying()) {
+            event.setMenu(false);
+        }
+    }
+
+    /** Set up the players in the game */
+    @EventHandler
+    public void onGameStart(GameStartEvent event) {
+        alive.addAll(team.getPlayers().stream().map(player -> player.getPlayer().getName()).collect(Collectors.toList()));
+        buildAndSendList();
+
+        if (alive.size() <= 1) {
+            game.stop();
+        }
+    }
+
+    /** Show death message to all game players */
+    @EventHandler
+    public void onPlayerDeath(GamePlayerDeathEvent event) {
+        event.getViewers().addAll(team.getPlayers());
+    }
+
+    /** Handle raw player death's */
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        deadPlayer(event.getEntity());
+
+        if (alive.size() == 1) {
+            SchedulerUtil.runSync(() -> {
+                if (game.getStage().isPlaying()) {
+                    new GamePlayerWinEvent(game, game.getPlaying().iterator().next()).call();
+                }
+            }, 5L);
+        }
+        else if (alive.size() == 0) {
+            if (game.getStage().isPlaying()) {
+                game.stop();
+            }
+        }
+    }
+
+    /** When players leave count that as a death */
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        deadPlayer(event.getPlayer());
+    }
+
+    /** Handle the dead player */
+    public void deadPlayer(Player name) {
+        if (alive.remove(name.getName())) {
+            dead.add(name.getName());
+            buildAndSendList();
+
+            if (game.getPlayer(name) != null) {
+                ((NodePlayer) game.getPlayer(name)).joinTeam(null);
+            }
+        }
+    }
+
+    /** Build the sidebar and send it to the players */
+    public void buildAndSendList() {
+        game.getSidebarGoals().clear();
+
+        alive.forEach(name -> game.addStaticGoal(name, "&a" + name));
+        dead.forEach(name -> game.addStaticGoal(name, "&c&m" + name));
+
+        game.getPlaying().forEach(player -> game.getScoreboardFactory().setGameSidebar((NodePlayer) player));
     }
 }
--- src/main/java/net/year4000/mapnodes/gamemodes/skywars/SkywarsConfig.java
+++ src/main/java/net/year4000/mapnodes/gamemodes/skywars/SkywarsConfig.java
@@ -1,14 +1,22 @@
 package net.year4000.mapnodes.gamemodes.skywars;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import net.year4000.mapnodes.api.game.modes.GameModeConfig;
 import net.year4000.mapnodes.api.game.modes.GameModeConfigName;
 import net.year4000.mapnodes.exceptions.InvalidJsonException;
+import net.year4000.mapnodes.messages.Msg;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 @Data
 @GameModeConfigName("skywars")
 public class SkywarsConfig implements GameModeConfig {
+    @SerializedName("players_team")
+    private String playersTeam = null;
+
     @Override
     public void validate() throws InvalidJsonException {
+        checkArgument(playersTeam != null, Msg.util("skywars.error.team_null"));
     }
 }
