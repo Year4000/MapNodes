c24779684a948c0a53c47f5e8afce0037f0594c6
Joshua Rodriguez <ewized@gmail.com>
2014-10-02 11:07:59 -0700

    Added Locales constant class.

---
+++ src/main/java/net/year4000/mapnodes/messages/Locales.java
@@ -0,0 +1,17 @@
+package net.year4000.mapnodes.messages;
+
+/**
+ * This will handle the locale constants that are used in MapNodes.
+ * Having the locales here will allow translators to know what keys to fill out.
+ * It will also make this the one spot to change locale keys if needed.
+ *
+ * The name of the constant will be the key name in upper underscore format.
+ *
+ * public static final String KEY_NAME = "key.name";
+ */
+public final class Locales {
+    // System
+    public static final String NAME = "";
+
+    // Public
+}
