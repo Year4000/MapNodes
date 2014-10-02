f5e6cb84a1c6f6b9092da0261b49f08673197408
Joshua Rodriguez <ewized@gmail.com>
2014-10-02 11:34:08 -0700

    Added creature spawn event

--- src/main/java/net/year4000/mapnodes/game/regions/events/CreatureSpawn.java
+++ src/main/java/net/year4000/mapnodes/game/regions/events/CreatureSpawn.java
@@ -4,8 +4,25 @@ import net.year4000.mapnodes.game.regions.EventType;
 import net.year4000.mapnodes.game.regions.EventTypes;
 import net.year4000.mapnodes.game.regions.RegionEvent;
 import net.year4000.mapnodes.game.regions.RegionListener;
-import org.bukkit.event.Listener;
+import net.year4000.mapnodes.game.regions.types.Point;
+import net.year4000.mapnodes.utils.typewrappers.EntityTypeList;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 
 @EventType(EventTypes.CREATURE_SPAWN)
 public class CreatureSpawn extends RegionEvent implements RegionListener {
+    private EntityTypeList<EntityType> creatures = new EntityTypeList<>();
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (!region.inZone(new Point(event.getLocation().toVector().toBlockVector()))) return;
+
+        if (isAllow() && creatures.contains(event.getEntityType()) || !isAllow() && !creatures.contains(event.getEntityType())) {
+            event.setCancelled(false);
+        }
+        else if (!isAllow() && creatures.contains(event.getEntityType())) {
+            event.setCancelled(true);
+        }
+    }
 }
