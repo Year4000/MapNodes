f0b8c9dea7412d0d447cefdfcc6645bfa5cfa618
Joshua Rodriguez <ewized@gmail.com>
2014-10-02 11:45:58 -0700

    Added Build Event

--- src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
+++ src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
@@ -1,11 +1,38 @@
 package net.year4000.mapnodes.game.regions.events;
 
+import net.year4000.mapnodes.api.game.GamePlayer;
 import net.year4000.mapnodes.game.regions.EventType;
 import net.year4000.mapnodes.game.regions.EventTypes;
 import net.year4000.mapnodes.game.regions.RegionEvent;
 import net.year4000.mapnodes.game.regions.RegionListener;
-import org.bukkit.event.Listener;
+import net.year4000.mapnodes.game.regions.types.Point;
+import net.year4000.mapnodes.utils.typewrappers.MaterialList;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.Arrays;
 
 @EventType(EventTypes.BUILD)
 public class Build extends RegionEvent implements RegionListener {
+    private MaterialList<Material> blocks = new MaterialList<>(Arrays.asList(Material.values()));
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        if (!region.inZone(new Point(event.getBlockPlaced().getLocation().toVector().toBlockVector()))) return;
+
+        GamePlayer player = region.getGame().getPlayer(event.getPlayer());
+        Material material = event.getBlockPlaced().getType();
+
+        if (applyToPlayer(player)) {
+            if (isAllow() && blocks.contains(material) || !isAllow() && !blocks.contains(material)) {
+                event.setCancelled(false);
+            }
+            else if (!isAllow() && blocks.contains(material)) {
+                event.setCancelled(true);
+            }
+
+            runGlobalEventTasks(player);
+        }
+    }
 }
