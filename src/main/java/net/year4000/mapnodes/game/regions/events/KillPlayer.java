7710837802a3007f63f9d616e99cd5304a879c6c
Joshua Rodriguez <ewized@gmail.com>
2014-10-09 21:01:32 -0700

    Fix npe in kill_player region event

--- src/main/java/net/year4000/mapnodes/game/regions/events/KillPlayer.java
+++ src/main/java/net/year4000/mapnodes/game/regions/events/KillPlayer.java
@@ -13,6 +13,8 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public class KillPlayer extends RegionEvent implements RegionListener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
+        if (event.getEntity().getKiller() == null) return;
+
         if (!region.inZone(new Point(event.getEntity().getKiller().getLocation().toVector().toBlockVector()))) return;
 
         GamePlayer player = region.getGame().getPlayer(event.getEntity().getKiller());
