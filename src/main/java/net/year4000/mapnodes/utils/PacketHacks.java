c2d1556fce2f6c91cf26d7606eff3ff2f7e23e02
Joshua Rodriguez <ewized@gmail.com>
2014-10-14 09:56:16 -0700

    Keep reviving player if fail kick player

--- src/main/java/net/year4000/mapnodes/utils/PacketHacks.java
+++ src/main/java/net/year4000/mapnodes/utils/PacketHacks.java
@@ -1,6 +1,7 @@
 package net.year4000.mapnodes.utils;
 
 import net.minecraft.server.v1_7_R4.Packet;
+import net.year4000.mapnodes.messages.Msg;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_7_R4.CraftServer;
 import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
@@ -11,7 +12,19 @@ public final class PacketHacks {
     /** Re-spawn a dead player */
     public static void respawnPlayer(Player player) {
         CraftPlayer craftPlayer = (CraftPlayer) player;
-        ((CraftServer) Bukkit.getServer()).getServer().getPlayerList().moveToWorld(craftPlayer.getHandle(), 0, false);
+        final int MAX = 64;
+        int counter = 0;
+
+        // Keep forcing respawn if else fail kick player.
+        while (craftPlayer.isDead()) {
+            if (counter >= MAX) {
+                craftPlayer.kickPlayer(Msg.locale(player, "error.cmd.error"));
+                break;
+            }
+
+            ((CraftServer) Bukkit.getServer()).getServer().getPlayerList().moveToWorld(craftPlayer.getHandle(), 0, false);
+            ++counter;
+        }
     }
 
     /** Set the tablist header and footer */
