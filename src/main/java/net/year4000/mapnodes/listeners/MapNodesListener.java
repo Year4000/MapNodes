82729d99a271228d690543a29ba5d361749f4abf
Joshua Rodriguez <ewized@gmail.com>
2014-10-16 20:14:23 -0700

    Check if player does not have VIP and show shop page

--- src/main/java/net/year4000/mapnodes/listeners/MapNodesListener.java
+++ src/main/java/net/year4000/mapnodes/listeners/MapNodesListener.java
@@ -39,7 +39,7 @@ public final class MapNodesListener implements Listener {
     public void onJoin(PlayerLoginEvent event) {
         NodeGame game = (NodeGame) MapNodes.getCurrentGame();
         if (game.getPlayers().count() > game.getMaxPlayers() + game.getMaxPlayers() / 2) {
-            event.disallow(PlayerLoginEvent.Result.KICK_FULL, Msg.locale(event.getPlayer(), "server.full"));
+            event.disallow(PlayerLoginEvent.Result.KICK_FULL, Msg.locale(event.getPlayer(), "server.full") + '\n' + Msg.locale(event.getPlayer(), "team.select.non_vip_url"));
         }
     }
 
