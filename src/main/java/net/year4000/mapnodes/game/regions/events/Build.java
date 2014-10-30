2e46a792af0c390b1b009d192816cdf5542c2ee3
Joshua Rodriguez <ewized@gmail.com>
2014-10-30 13:54:14 -0700

    Gravity blocks have gravity all the time

--- src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
+++ src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
@@ -6,17 +6,26 @@ import net.year4000.mapnodes.game.regions.EventTypes;
 import net.year4000.mapnodes.game.regions.RegionEvent;
 import net.year4000.mapnodes.game.regions.RegionListener;
 import net.year4000.mapnodes.game.regions.types.Point;
+import net.year4000.mapnodes.utils.SchedulerUtil;
 import net.year4000.mapnodes.utils.typewrappers.MaterialList;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @EventType(EventTypes.BUILD)
 public class Build extends RegionEvent implements RegionListener {
+    private transient List<Integer> gravityEntities = new ArrayList<>();
+    private transient List<Block> gravityBlocks = new ArrayList<>();
     private MaterialList<Material> blocks = new MaterialList<>(Arrays.asList(Material.values()));
     private boolean gravity = false;
 
@@ -41,11 +50,38 @@ public class Build extends RegionEvent implements RegionListener {
             if (gravity && blocks.contains(material) && event.getBlockPlaced().getRelative(BlockFace.DOWN).isEmpty()) {
                 FallingBlock block = event.getBlock().getWorld().spawnFallingBlock(event.getBlockPlaced().getLocation(), material, event.getBlockPlaced().getData());
                 block.setDropItem(false);
+                gravityEntities.add(block.getEntityId());
                 event.getBlock().setType(Material.AIR);
+            }
+            else if (gravity && blocks.contains(material)) {
+                gravityBlocks.add(event.getBlock());
             }
 
             runGlobalEventTasks(player);
             runGlobalEventTasks(event.getBlock().getLocation());
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockLand(EntityChangeBlockEvent event) {
+        if (event.getEntity() instanceof org.bukkit.entity.FallingBlock) {
+            if (event.getBlock().getType() == Material.AIR && gravityEntities.remove((Integer) event.getEntity().getEntityId())) {
+                SchedulerUtil.runSync(() -> {
+                    Location loc = event.getBlock().getLocation();
+                    gravityBlocks.add(loc.getBlock());
+                });
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBreak(BlockBreakEvent event) {
+        if (gravityBlocks.remove(event.getBlock().getRelative(BlockFace.UP))) {
+            Block block = event.getBlock().getRelative(BlockFace.UP);
+            FallingBlock fallingBlock = event.getBlock().getWorld().spawnFallingBlock(block.getLocation(), block.getType(), block.getData());
+            fallingBlock.setDropItem(false);
+            gravityEntities.add(fallingBlock.getEntityId());
+            event.getBlock().setType(Material.AIR);
         }
     }
 }
