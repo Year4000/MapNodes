fa751d1f11e8f279eaeced03c2d76001e31de99e
Joshua Rodriguez <ewized@gmail.com>
2014-10-29 18:17:55 -0700

    Make blocks have gravity for #43

--- src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
+++ src/main/java/net/year4000/mapnodes/game/regions/events/Build.java
@@ -8,6 +8,7 @@ import net.year4000.mapnodes.game.regions.RegionListener;
 import net.year4000.mapnodes.game.regions.types.Point;
 import net.year4000.mapnodes.utils.typewrappers.MaterialList;
 import org.bukkit.Material;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
 
@@ -16,6 +17,7 @@ import java.util.Arrays;
 @EventType(EventTypes.BUILD)
 public class Build extends RegionEvent implements RegionListener {
     private MaterialList<Material> blocks = new MaterialList<>(Arrays.asList(Material.values()));
+    private boolean gravity = false;
 
     @EventHandler
     public void onPlace(BlockPlaceEvent event) {
@@ -27,6 +29,13 @@ public class Build extends RegionEvent implements RegionListener {
         if (applyToPlayer(player)) {
             if ((isAllow() && blocks.contains(material)) || (!isAllow() && !blocks.contains(material))) {
                 event.setCancelled(false);
+
+                // Make the block have gravity when placed
+                if (gravity) {
+                    FallingBlock block = event.getBlock().getWorld().spawnFallingBlock(event.getBlockPlaced().getLocation(), material, event.getBlockPlaced().getData());
+                    block.setDropItem(false);
+                    event.getBlock().setType(Material.AIR);
+                }
             }
             else if ((isAllow() && !blocks.contains(material)) || (!isAllow() && blocks.contains(material))) {
                 event.setCancelled(true);
