e6e76cf4c495d3369164d3786a5c78dc02101b1f
kacgal <kacgal@gmail.com>
2014-12-11 18:05:35 +0100

    Really fix monument bug

--- src/main/java/net/year4000/mapnodes/gamemodes/destory/Destroy.java
+++ src/main/java/net/year4000/mapnodes/gamemodes/destory/Destroy.java
@@ -50,7 +50,6 @@ public class Destroy extends GameModeTemplate implements GameMode {
         });
     }
 
-    @EventHandler (priority = EventPriority.HIGH)
     public void onBreak(BlockBreakEvent event) {
         GamePlayer player = game.getPlayer(event.getPlayer());
         NodeTeam team = ((NodeTeam) player.getTeam());
@@ -58,7 +57,6 @@ public class Destroy extends GameModeTemplate implements GameMode {
         event.setCancelled(destroyTarget(player, team, event.getBlock()));
     }
 
-    @EventHandler (priority = EventPriority.HIGH)
     public void onBreak(EntityExplodeEvent event) {
         GamePlayer player = game.getPlayer(Bukkit.getOnlinePlayers().iterator().next());
         NodeTeam team = ((NodeTeam) player.getTeam());
