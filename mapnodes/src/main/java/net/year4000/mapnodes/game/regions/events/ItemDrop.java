424a6dbb6a15aeb22fffbc069db13e4aca738178
Joshua Rodriguez <ewized@gmail.com>
2015-07-17 16:10:17 -0700

    Mkae ItemDrop event run in sync

--- mapnodes/src/main/java/net/year4000/mapnodes/game/regions/events/ItemDrop.java
+++ mapnodes/src/main/java/net/year4000/mapnodes/game/regions/events/ItemDrop.java
@@ -4,12 +4,14 @@
 
 package net.year4000.mapnodes.game.regions.events;
 
+import com.google.common.collect.ImmutableList;
 import net.year4000.mapnodes.api.MapNodes;
 import net.year4000.mapnodes.api.events.game.GameClockEvent;
 import net.year4000.mapnodes.api.game.regions.EventType;
 import net.year4000.mapnodes.api.game.regions.RegionListener;
 import net.year4000.mapnodes.api.game.regions.EventTypes;
 import net.year4000.mapnodes.game.regions.RegionEvent;
+import net.year4000.mapnodes.utils.SchedulerUtil;
 import net.year4000.mapnodes.utils.typewrappers.MaterialList;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
@@ -40,13 +42,21 @@ public class ItemDrop extends RegionEvent implements RegionListener {
 
     @EventHandler
     public void onStart(GameClockEvent event) {
-        // Cycle through the world and remove items that should not be their
-        MapNodes.getCurrentWorld().getEntitiesByClass(Item.class).stream()
-            .forEach(item -> {
-                Material material = item.getItemStack().getType();
-                if ((isAllow() && !items.contains(material)) || (!isAllow() && items.contains(material))) {
-                    item.remove();
-                }
-            });
+        // Force to run in sync
+        SchedulerUtil.runSync(() -> {
+            // Create a copy of the items
+            ImmutableList<Item> entities = new ImmutableList.Builder<Item>()
+                .addAll(MapNodes.getCurrentWorld().getEntitiesByClass(Item.class))
+                .build();
+
+            // Cycle through the world and remove items that should not be their
+            entities.stream()
+                .forEach(item -> {
+                    Material material = item.getItemStack().getType();
+                    if ((isAllow() && !items.contains(material)) || (!isAllow() && items.contains(material))) {
+                        item.remove();
+                    }
+                });
+        });
     }
 }
