407fb3d254cdd32a5dfaca4231d3e51109ea3a6e
Joshua Rodriguez <ewized@gmail.com>
2015-09-27 08:02:10 -0700

    Set the biome to sky and use method to get the max height of the world

--- mapnodes/src/main/java/net/year4000/mapnodes/utils/NullGenerator.java
+++ mapnodes/src/main/java/net/year4000/mapnodes/utils/NullGenerator.java
@@ -5,14 +5,17 @@
 package net.year4000.mapnodes.utils;
 
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
 
 import java.util.Random;
 
 public class NullGenerator extends ChunkGenerator {
+
     @Override
     @SuppressWarnings("deprecation")
     public byte[] generate(World world, Random random, int x, int z) {
-        return new byte[16 * 16 * 256];
+        world.setBiome(x, z, Biome.SKY);
+        return new byte[16 * 16 * world.getMaxHeight()];
     }
 }
