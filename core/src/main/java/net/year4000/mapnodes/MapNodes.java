846fde3867ba34780a8ade5371c323fd5cdedafb
Joshua Rodriguez <ewized@gmail.com>
2016-09-15 14:48:36 -0700

    Add stages on what mapnodes needs to do

--- core/src/main/java/net/year4000/mapnodes/MapNodes.java
+++ core/src/main/java/net/year4000/mapnodes/MapNodes.java
@@ -1,11 +1,40 @@
 /*
  * Copyright 2016 Year4000. All Rights Reserved.
  */
-
 package net.year4000.mapnodes;
 
+import net.year4000.mapnodes.nodes.Node;
+import net.year4000.mapnodes.nodes.NodeFactory;
 import net.year4000.mapnodes.nodes.NodeManager;
 
+/** The system to handle the Maps and load their games */
 public interface MapNodes {
+  Settings SETTINGS = new Settings();
   NodeManager NODE_MANAGER = new NodeManager();
+
+  /** Get the bindings */
+  Bindings bindings();
+
+  /** Get the node factory */
+  NodeFactory nodeFactory();
+
+  /** Get the current node */
+  default Node currentNode() {
+    return NODE_MANAGER.getNode();
+  }
+
+  /** Load the MapNodes system */
+  default void load() {
+
+  }
+
+  /** Enable the MapNodes system */
+  default void enable() {
+
+  }
+
+  /** Unload the MapNodes system */
+  default void unload() {
+
+  }
 }
--- sponge/src/main/java/net/year4000/mapnodes/MapNodesPlugin.java
+++ sponge/src/main/java/net/year4000/mapnodes/MapNodesPlugin.java
@@ -5,10 +5,41 @@ package net.year4000.mapnodes;
 
 import net.year4000.mapnodes.nodes.NodeFactory;
 import net.year4000.mapnodes.nodes.SpongeNodeFactory;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameConstructionEvent;
+import org.spongepowered.api.event.game.state.GameLoadCompleteEvent;
+import org.spongepowered.api.event.game.state.GameStoppingEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "mapnodes")
-public class MapNodesPlugin {
-  private final Bindings bindings = new SpongeBindings();
-  private final NodeFactory nodeFactory = new SpongeNodeFactory();
+@Plugin(id = "mapnodes", version = "3.0.0-SNAPSHOT")
+public class MapNodesPlugin implements MapNodes {
+  private final SpongeBindings bindings = new SpongeBindings();
+  private final SpongeNodeFactory nodeFactory = new SpongeNodeFactory();
+
+  @Override
+  public Bindings bindings() {
+    return bindings;
+  }
+
+  @Override
+  public NodeFactory nodeFactory() {
+    return nodeFactory;
+  }
+
+  // Events for MapNodes
+
+  @Listener
+  public void onLoad(GameConstructionEvent event) {
+    load();
+  }
+
+  @Listener
+  public void onEnable(GameLoadCompleteEvent event) {
+    enable();
+  }
+
+  @Listener
+  public void onEnable(GameStoppingEvent event) {
+    unload();
+  }
 }
