f125a3d84453eadbfc76b32beaf2b730c9725fef
Joshua Rodriguez <ewized@gmail.com>
2016-09-15 17:36:47 -0700

    Print message to known what stage

--- core/src/main/java/net/year4000/mapnodes/MapNodes.java
+++ core/src/main/java/net/year4000/mapnodes/MapNodes.java
@@ -34,7 +34,9 @@ public interface MapNodes {
   /** Load the MapNodes system */
   default void load() {
     // Inject the javascript files into v8
+    System.out.println("Loading javascript files into v8 runtime");
     ImmutableSet.of("bindings.js", "game.js", "player.js", "team.js", "utils.js").forEach(file -> {
+      System.out.println("Loading javascript file: " + file);
       InputStream stream = MapNodes.class.getResourceAsStream("/js/" + file);
       try (BufferedReader buffer = new BufferedReader(new InputStreamReader(stream))) {
         String script = buffer.lines().collect(Collectors.joining("\n"));
@@ -44,18 +46,22 @@ public interface MapNodes {
       }
     });
     // Generate the maps
+    System.out.println("Generating map packages");
     nodeFactory().generatePackages();
   }
 
   /** Enable the MapNodes system */
   default void enable() {
+    System.out.println("Adding maps to queue");
     nodeFactory().packages().forEach(map -> {
+      System.out.println("Adding map: " + map.toString());
       // todo exception handling
       Node node = nodeFactory().create(map);
       NODE_MANAGER.addToQueue(node);
     });
     // Set up the first node if we can
     if (nodeFactory().packages().size() > 0) {
+      System.out.println("Creating first node");
       NODE_MANAGER.loadNextNode();
     }
   }
@@ -64,8 +70,10 @@ public interface MapNodes {
   default void unload() {
     // Unload the last map in the system
     if (nodeFactory().packages().size() > 0) {
+      System.out.println("Unloading current node");
       currentNode().unload();
     }
+    System.out.println("Releasing runtime bindings");
     bindings().release();
   }
 }
