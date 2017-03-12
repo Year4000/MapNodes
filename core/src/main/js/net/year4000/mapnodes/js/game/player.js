6041a5ce74b88847f9b3c28ac56e33795186b874
Joshua Rodriguez <ewized@gmail.com>
2017-03-12 10:35:04 -0700

    Create the instances of player staticly

--- core/src/main/js/net/year4000/mapnodes/js/game/player.js
+++ core/src/main/js/net/year4000/mapnodes/js/game/player.js
@@ -2,10 +2,34 @@
  * Copyright 2016 Year4000. All Rights Reserved.
  */
 
+/** Generates the player object */
 class Player {
 
   constructor(json) {
     this._meta = json;
+  }
+
+  /** Create the instance of this player */
+  static of(player) {
+    Conditions.not_null(player, 'player');
+    Conditions.is_true(typeof player === 'string');
+    if (_.size(player) > Facts.MAX_USERNAME_SIZE) { // must be uuid
+      return Player.of_uuid(player);
+    } else { // Must be username
+      return Player.of_username(player);
+    }
+  }
+
+  /** Generate the player meta from the uuid */
+  static of_uuid(uuid) {
+    Conditions.not_null(uuid, 'uuid');
+    return new Player($.bindings.player_meta_uuid(uuid));
+  }
+
+  /** Generate the player meta from a username */
+  static of_username(username) {
+    Conditions.not_null(username, 'username');
+    return new Player($.bindings.player_meta_username(username));
   }
 
   get uuid() {
