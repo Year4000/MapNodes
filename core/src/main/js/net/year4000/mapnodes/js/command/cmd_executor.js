081670324a89d7bfe20564de9370b75fadbec9fc
Joshua Rodriguez <ewized@gmail.com>
2017-03-23 07:01:48 -0700

    Stub out the command framework

---
+++ core/src/main/js/net/year4000/mapnodes/js/command/cmd_executor.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2017 Year4000. All Rights Reserved.
+ */
+
+/** The command executor is the one who is executating the commands */
+class CommandExecutor {
+
+}
---
+++ core/src/main/js/net/year4000/mapnodes/js/command/cmd_manager.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2017 Year4000. All Rights Reserved.
+ */
+
+/** The command manager that will handle processing commands and ect */
+class CommandManager {
+
+}
