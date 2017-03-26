8a49487d7e3d9f49c8277270205ec48588c78ad1
Joshua Rodriguez <ewized@gmail.com>
2017-03-26 10:16:33 -0700

    Remove v2 from v3

--- mapnodes/src/main/java/net/year4000/mapnodes/api/utils/Spectator.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2015 Year4000. All Rights Reserved.
- */
-
-package net.year4000.mapnodes.api.utils;
-
-public interface Spectator {
-}
--- mapnodes/src/main/java/net/year4000/mapnodes/game/scoreboard/TabListViewer.java
+++
@@ -1,4 +0,0 @@
-package net.year4000.mapnodes.game.scoreboard;
-
-public class TabListViewer {
-}
