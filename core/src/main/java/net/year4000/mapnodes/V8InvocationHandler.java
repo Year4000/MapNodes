a539ca8b77847d3895a7475ee9e292398bcb4d41
Joshua Rodriguez <ewized@gmail.com>
2017-08-12 10:34:52 -0700

    Lazy load the object, ignore memory leak right now

--- core/src/main/java/net/year4000/mapnodes/V8InvocationHandler.java
+++ core/src/main/java/net/year4000/mapnodes/V8InvocationHandler.java
@@ -9,20 +9,19 @@ import com.google.common.base.CaseFormat;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalListener;
 import net.year4000.utilities.Conditions;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
 
 /** Creates a proxy system to access the Javascript engine */
 class V8InvocationHandler implements InvocationHandler {
   /** The object that the methods will execute the Javascript function */
   private final V8 engine;
   private final String lookup;
+  private V8Object object;
   /** The method name cache for the translation of java method names to the javascript function names */
   private final LoadingCache<String, String> methodNameCache = CacheBuilder.<String, String>newBuilder()
     .concurrencyLevel(1) // You can only access the v8 engine with only one thread
@@ -30,22 +29,6 @@ class V8InvocationHandler implements InvocationHandler {
       @Override
       public String load(String key) throws Exception {
         return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, key);
-      }
-    });
-  /** Cache the V8Object that is grabbed from the v8 engine */
-  private final LoadingCache<String, V8Object> objectCache = CacheBuilder.<String, V8Object>newBuilder()
-    .concurrencyLevel(1) // You can only access the v8 engine with only one thread
-    .expireAfterAccess(1, TimeUnit.SECONDS)
-    .removalListener((RemovalListener<String, V8Object>) cached -> {
-      V8Object value = cached.getValue();
-      if (value != null && !value.isReleased()) {
-        value.release();
-      }
-    })
-    .build(new CacheLoader<String, V8Object>() {
-      @Override
-      public V8Object load(String key) throws Exception {
-        return engine.executeObjectScript(key);
       }
     });
 
@@ -64,8 +47,11 @@ class V8InvocationHandler implements InvocationHandler {
         .bindTo(proxy)
         .invokeWithArguments(args);
     }
+    if (object == null || object.isReleased()) { // Lazy load the object handling the functions
+      object = engine.executeObjectScript(lookup);
+    }
     // Cache the conversion of the java method name to the js function name
     // It is O(n) on first run, n being the length of the string then O(1) if the cache is present
-    return objectCache.getUnchecked(lookup).executeJSFunction(methodNameCache.get(method.getName()), args);
+    return object.executeJSFunction(methodNameCache.get(method.getName()), args);
   }
 }
