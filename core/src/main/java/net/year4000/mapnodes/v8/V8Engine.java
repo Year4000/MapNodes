430f4bf3b02434335fdf299d187ff9271c8c22b1
Joshua Rodriguez <ewized@gmail.com>
2018-04-25 08:52:31 -0700

    Start working on implementing out own v8 bridge system

---
+++ core/src/main/java/net/year4000/mapnodes/v8/V8Engine.java
@@ -0,0 +1,16 @@
+package net.year4000.mapnodes.v8;
+
+import java.nio.file.Paths;
+
+public class V8Engine {
+  public native boolean startVm();
+
+
+  public static void main(String[] args) {
+    System.out.println(Paths.get(".").toAbsolutePath());
+    System.load("/Users/joshua/Documents/year4000/MapNodes/core/build/libs/mapnodes/shared/libmapnodes.dylib");
+
+    V8Engine v8 = new V8Engine();
+    v8.startVm();
+  }
+}
