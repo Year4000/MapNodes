2f1ca6341241a084d655a13a8559a137ea8a7d25
Joshua Rodriguez <ewized@gmail.com>
2020-03-28 18:50:26 -0700

    Fix formatting

--- core/src/main/js/mapnodes/hooks/hooks.js
+++ core/src/main/js/mapnodes/hooks/hooks.js
@@ -2,11 +2,11 @@
  * Copyright 2020 Year4000. All Rights Reserved.
  */
 
- /**
-  * MapNode hooks is a system that exposed publicly
-  * accessed variables and functions with out needing
-  * to rebind this if needed.
-  *
-  * They are primarily used for map listners and game modes
-  */
+/**
+ * MapNode hooks is a system that exposed publicly
+ * accessed variables and functions with out needing
+ * to rebind this if needed.
+ *
+ * They are primarily used for map listners and game modes
+ */
 export { useMapNodes } from './useMapNodes.js'
